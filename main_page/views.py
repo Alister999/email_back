5b76fd8a0fd02f224e36e0a376f886ef909bebcd
Aleksandr Sidorov <a.sidorov@teleguard.com>
2025-06-04 10:10:32 +0200

    done basis page with error btn

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,12 @@
+from http.client import HTTPException
+
+from django.http import HttpRequest, HttpResponse, HttpResponseServerError
 from django.shortcuts import render
 
 # Create your views here.
 
 def show_start(request):
     return render(request, 'main_page/index.html')
+
+def error_view(request):
+    return HttpResponseServerError()#HttpResponse()
